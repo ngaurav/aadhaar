267219ea8c5f6a46f90b68f8de09f9bbd5658f92
nishant <ng.nishant.ng@gmail.com>
2016-08-09 00:32:00 +0530

    gunicorn_start

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 NAME="bootcamp"                              #Name of the application (*)
-DJANGODIR=/root/public_html/finpulse.com    # Django project directory (*)
+DJANGODIR=/home/nishant/public_html/provider    # Django project directory (*)
 NUM_WORKERS=4                                             # how many worker processes should Gunicorn spawn (*)
 DJANGO_SETTINGS_MODULE=bootcamp.settings             # which settings file should Django use (*)
 DJANGO_WSGI_MODULE=bootcamp.wsgi                     # WSGI module name (*)
-LOGFILE=/root/public_html/logs/gunicorn.log
+LOGFILE=/home/nishant/public_html/logs/gunicorn.log
 echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source /root/ve/bin/activate
+source /home/nishant/ve/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec /root/ve/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec /home/nishant/ve/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --log-level=debug \
-  --bind unix:/root/public_html/finpulse.com/finpulse.sock \
+  --bind unix:/home/nishant/public_html/provider/provider.sock \
   --log-file=$LOGFILE 2>>$LOGFILE
 
