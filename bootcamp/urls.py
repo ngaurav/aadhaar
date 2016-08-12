5727065013039428a939d4e8044e4799e7fde6c3
nishant <ng.nishant.ng@gmail.com>
2016-08-12 21:14:07 +0530

    urls

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allaccess.urls')),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 
     url(r'^login', auth_views.login, {'template_name': 'core/cover.html'},
         name='login'),
